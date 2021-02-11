428484090b8e2dd0ab9ad9e8e7478969f8a4a88c
ygor_verissimo <ygor_verissimo@hotmail.com>
2021-02-11 14:46:09 -0300

    atualizando documentos segundo as aulas

---
+++ JS/functions.js
@@ -0,0 +1,17 @@
+// creat functions 
+
+//function - statement
+
+function creatPhrases()  {
+    console.log('Estudar é importante')
+    console.log('O segredodo sucesso é a persistência')
+    console.log('A revisão é a mãe do aprendizado')
+
+}
+
+//Executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+creatPhrases()
+console.log('fim do programa')
+
---
+++ JS/functions_01.js
@@ -0,0 +1,13 @@
+// function expression
+// function anonymous
+
+// parâmentros or parameters
+const sum = function(number1,number2){
+ let total = number1 + number2
+ return total   
+}
+let number1 = 100
+let number2 = 200
+console.log(`O Numero1 é:${number1}`)
+console.log(`O Numero2 é:${number2}`)
+console.log(`A soma dos dois valores é:${sum(number1, number2)}`)
